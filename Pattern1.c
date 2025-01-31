cb54e1a3356fc1bd91be3af4d0cc567fc584749c
Amaan Mujawar <amaanmujawar175@gmail.com>
2025-01-31 20:50:14 +0530

    Sixth Commit

---
+++ Pattern1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, n;
+
+    printf("Enter the value for N: ");
+    scanf("%d", &n);
+
+    i = n;
+
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+            printf(" * ");
+            
+        printf(" # \n");
+    }
+    return 0;
+}
